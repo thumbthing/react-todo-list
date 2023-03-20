5f9c9652b3805b4cac8e1984ebb1c62538a849f0
thumbthing <ilscc0902@gmail.com>
2023-03-20 17:34:44 +0900

    feat: List Component add

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,12 +7,15 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ( { todo, onRemove, onToggle } ) => {
+const TodoListItem = ( { todo, onRemove, onToggle, style } ) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className='TodoListItem'>
-      <div className={cn( 'checkbox' , { checked })} onClick = {() => onToggle(id)}>
+    <div className='TodoListItem-virtualized' style={style}>
+      <div 
+        className={cn( 'checkbox' , { checked })} 
+        onClick = {() => onToggle(id)}
+      >
         { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank /> }
         <div className='text'>{text}</div>
       </div>
